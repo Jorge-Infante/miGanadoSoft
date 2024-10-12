7f16cfb7ebcd8ee941fbdeff836ce4364b2c8fae
Jorge Infante <jorge.infante@ssc.com.co>
2024-10-12 01:09:46 -0500

    serializadores llaves

--- applications/animal/api/serializers.py
+++ applications/animal/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from applications.animal.models import Breed, Species, Animal, Status
+from applications.treatment.models import Treatment
 from applications.treatment.api.serializers import TreatmentSerializer
 
 class BreedSerializer(serializers.ModelSerializer):
@@ -18,11 +19,21 @@ class StatusSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class AnimalSerializer(serializers.ModelSerializer):
-    breed = BreedSerializer()
-    species = SpeciesSerializer()
-    status = StatusSerializer()
-    treatments = TreatmentSerializer(many=True, read_only=True, source='treatment_set')
-
+    breed = serializers.PrimaryKeyRelatedField(queryset=Breed.objects.all())
+    species = serializers.PrimaryKeyRelatedField(queryset=Species.objects.all())
+    status = serializers.PrimaryKeyRelatedField(queryset=Status.objects.all())
+    treatment = serializers.PrimaryKeyRelatedField(many=True, queryset=Treatment.objects.all())
     class Meta:
         model = Animal
-        fields = ["name","number","weight","file","image","breed", "species","status","treatments"]
+        fields = ["name","number","weight","file","image","breed", "species","status","treatment"]
+    
+    def to_representation(self, instance):
+        """Modificar la representación para el GET."""
+        # Llamamos al método original para obtener los datos serializados
+        representation = super().to_representation(instance)
+        # Reemplazamos los IDs de 'medicine' con los datos completos
+        representation['breed'] = BreedSerializer(instance.breed).data
+        representation['species'] = SpeciesSerializer(instance.species).data
+        representation['status'] = StatusSerializer(instance.status).data
+        representation['treatment'] = TreatmentSerializer(instance.treatment.all(), many=True).data
+        return representation
--- applications/treatment/api/serializers.py
+++ applications/treatment/api/serializers.py
@@ -7,6 +7,15 @@ class MedicineSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TreatmentSerializer(serializers.ModelSerializer):
+    medicine = serializers.PrimaryKeyRelatedField(many=True, queryset=Medicine.objects.all())
     class Meta:
         model = Treatment
-        fields = '__all__'
+        fields = ['id','name','medicine']
+    
+    def to_representation(self, instance):
+        """Modificar la representación para el GET."""
+        # Llamamos al método original para obtener los datos serializados
+        representation = super().to_representation(instance)
+        # Reemplazamos los IDs de 'medicine' con los datos completos
+        representation['medicine'] = MedicineSerializer(instance.medicine.all(), many=True).data
+        return representation
